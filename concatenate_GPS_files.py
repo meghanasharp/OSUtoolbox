fix time error in gps time calculation in concatenate_GPS_files.py

concatenate_GPS_files.py
@@ -54,7 +54,7 @@ def concat_gps_csv_files(path_to_folder=None, path_to_save=None, output_filename
     df = df.reset_index(drop=True)
     
     #create time array in local and utc time    
-    df['time_utc'] = dt.datetime.strptime("01/01/22", "%m/%d/%y") + pd.to_timedelta(df.day_of_year,'d')  + pd.to_timedelta(df.decimal_hour,'h')
+    df['time_utc'] = dt.datetime.strptime("01/01/22", "%m/%d/%y") + pd.to_timedelta(df.day_of_year-1,'d')  + pd.to_timedelta(df.decimal_hour,'h') #do we need to substract one day?? ask christian
     df['time_utc'] = np.round(df['time_utc'].astype(np.int64), -9).astype('datetime64[ns]')
     df['time_local'] = df['time_utc'] + dt.timedelta(hours = -3)
     