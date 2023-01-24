ddc5c0b55102500641f971cd1cf15a622afc40c1
Celia Trunz <30932911+cctrunz@users.noreply.github.com>
2023-01-24 13:10:28 -0800

    Update ShallowIceApproximation.py

--- ShallowIceApproximation.py
+++ ShallowIceApproximation.py
@@ -11,6 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #from matplotlib.pyplot import cm
 import numpy as np
+import seaborn as sns
 
 #load profile data
 
@@ -103,7 +104,9 @@ alpha = df.alpha.rolling(window=60,center=True).mean()
 #2. always take a timestep that will create a small enough ice motion compare to delta x
 
 number = 3
-fig,ax = plt.subplots(number, sharex=True)
+fig,ax = plt.subplots(number, sharex=True, figsize=(15,10))
+
+ax[2].invert_xaxis()
 
 ax[0].plot(profile.distance, profile.arcticdem_10m_ice_surface_elevation_masl,label='ice surface elevation - arctic dem 32m')
 ax[0].fill_between(profile.distance, profile.arcticdem_10m_ice_surface_elevation_masl, profile.bedmachine_bed_elevation_masl, alpha=0.3)
@@ -114,14 +117,18 @@ ax[0].set_ylabel('(m.a.s.l.)')
 
 ax[1].plot(profile.distance,profile.promice_velocity_mperday*365, label='promice velocity', color='purple', linestyle='--')
 
-color=plt.cm.PuRd(np.linspace(0,1,len(cuffey.A)))
+selected_temperature = [0,-10,-20,-40]
+
+color=plt.cm.coolwarm_r(np.linspace(0,1,len(selected_temperature)))
 
 
-for i,A in enumerate(cuffey.A):
+
+for i,temperature in enumerate(selected_temperature): #enumerate(cuffey['T']):
 #
     taub = rho_ice *g*H*alpha
-    temperature = cuffey['T'][i]
-    
+    #temperature = cuffey['T'][i]
+    A = cuffey.A[cuffey['T']==temperature].values[0]
+    print(A)
     surface_velocity =  (ub + 2*A/(n+1) *taub**n *H)*3600*24 
     #column[i]='T='+str(cuffey['T'])
     #us_approx= H**4 *alpha**3#*3600*24
@@ -141,9 +148,99 @@ ax[2].axvspan(lake_area[0],lake_area[len(lake_area)-1], alpha=0.3, color='cyan')
 ax[2].axvspan(radar_area[0],radar_area[len(radar_area)-1], alpha=0.3, color='grey')
 ax[2].set_ylabel('(-)')
 
+ax[2].set_xlim(25000,0)
+
 ax[2].set_xlabel('Distance from ice divide (m)')
+
+sns.despine(ax=ax[0], bottom=True)
+sns.despine(ax=ax[1], bottom=True)
+sns.despine(ax=ax[2], bottom=False)
+ax[0].tick_params(bottom=False, labelbottom=False)
+ax[1].tick_params(bottom=False, labelbottom=False)
 
 for i in np.arange(number):
     ax[i].legend(prop={'size': 6})
+
+
+#%% AGU plots
+
+
+fig,ax = plt.subplots(sharex=True, figsize=(5,2))
+ax.invert_xaxis()
+
+ax.plot(profile.distance, profile.arcticdem_10m_ice_surface_elevation_masl,label='ice surface elevation - arctic dem 32m')
+ax.fill_between(profile.distance, profile.arcticdem_10m_ice_surface_elevation_masl, profile.bedmachine_bed_elevation_masl, alpha=0.3)
+ax.plot(profile.distance, profile.bedmachine_bed_elevation_masl, label='bed elevation - bedmachine')
+ax.axvspan(lake_area[0],lake_area[len(lake_area)-1], alpha=0.3, color='cyan', label='minimum lake extent')
+ax.axvspan(radar_area[0],radar_area[len(radar_area)-1], alpha=0.3, color='grey', label='radar extent')
+ax.set_ylabel('(m.a.s.l.)')
+ax.set_xlim(25000,0)
+ax.set_xlabel('Distance from ice divide (m)')
+
+sns.despine(ax=ax, bottom=False)
+ax.tick_params(bottom=False, labelbottom=False)
+plt.savefig('profile_agu.pdf')
+
+################################################################
+fig,ax = plt.subplots(sharex=True, figsize=(5,1))
+ax.invert_xaxis()
+
+ax.plot(profile.distance,profile.promice_velocity_mperday*365, label='promice velocity', color='purple', linestyle='--')
+selected_temperature = [0,-10,-20,-40]
+color=plt.cm.coolwarm_r(np.linspace(0,1,len(selected_temperature)))
+
+for i,temperature in enumerate(selected_temperature): #enumerate(cuffey['T']):
+#
+    taub = rho_ice *g*H*alpha
+    #temperature = cuffey['T'][i]
+    A = cuffey.A[cuffey['T']==temperature].values[0]
+    print(A)
+    surface_velocity =  (ub + 2*A/(n+1) *taub**n *H)*3600*24 
+    #column[i]='T='+str(cuffey['T'])
+    #us_approx= H**4 *alpha**3#*3600*24
+    ax.plot(profile.distance,surface_velocity*365, label='A (%s°C)'%temperature, color=color[i])  
+
+ax.set_ylabel('(m/year)')
+ax.set_ylim(-1,50)
+ax.set_xlim(25000,0)
+ax.set_xlabel('Distance from ice divide (m)')
+sns.despine(ax=ax)
+for i in np.arange(number):
+    ax.legend(prop={'size': 6})
+plt.savefig('velocity_agu.pdf')
+
+ ##############################################################
+fig,ax = plt.subplots(sharex=True, figsize=(5,1))
+ax.invert_xaxis()
+
+selected_temperature = [0,-10,-20,-40]
+color=plt.cm.coolwarm_r(np.linspace(0,1,len(selected_temperature)))
+
+for i,temperature in enumerate(selected_temperature): #enumerate(cuffey['T']):
+#
+    taub = rho_ice *g*H*alpha
+    #temperature = cuffey['T'][i]
+    A = cuffey.A[cuffey['T']==temperature].values[0]
+    print(A)
+    surface_velocity =  (ub + 2*A/(n+1) *taub**n *H)*3600*24 
+    #column[i]='T='+str(cuffey['T'])
+    #us_approx= H**4 *alpha**3#*3600*24
+    ax.plot(profile.distance,surface_velocity/profile.promice_velocity_mperday, label='A (%s°C)'%temperature, color=color[i])  
+
+ax.set_ylabel('(fraction)')
+ax.set_ylim(0,1)
+ax.set_xlim(25000,0)
+ax.set_xlabel('Distance from ice divide (m)')
+sns.despine(ax=ax)
+for i in np.arange(number):
+    ax.legend(prop={'size': 6})
+
+ax.axvspan(lake_area[0],lake_area[len(lake_area)-1], alpha=0.3, color='cyan')
+plt.savefig('fraction_creep_agu.pdf')
+
+
+
+
+
 
 
