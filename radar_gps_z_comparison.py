0254544a88b38646797d8ec8a774656e686d122d
kasiii7 <kirillpskov@gmail.com>
2023-02-06 15:31:59 -0800

    working time shift

--- radar_gps_z_comparison.py
+++ radar_gps_z_comparison.py
@@ -8,7 +8,7 @@ Created on Fri Nov 11 17:17:03 2022
 
 
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import timedelta
 import numpy as np
@@ -49,7 +49,7 @@ def plot_2D(x,y,z,
 lake = pd.read_csv('G:/Shared drives/6 Greenland Europa Hiawatha Projects/Lake Europa/Previous_dataset/naqlk_2014_water_detected.csv')
 
 # GPS data from the radar instrument. exported by Kirill
-radar = pd.read_csv('G:/Shared drives/6 Greenland Europa Hiawatha Projects/Lake Europa/Radar/radar_GPS_datetime_stereographic.csv',
+radar = pd.read_csv('G:/Shared drives/6 Greenland Europa Hiawatha Projects/Lake Europa/Radar/radar_GPS_datetime_stereographic_2dinterp.csv',
                     index_col='datetime',
                     parse_dates=True)
 
@@ -97,14 +97,14 @@ for i,timeshift in enumerate(table.time_shift):
         radar['index_shift'][selection][~mask] = radar.index[selection][~mask] + timedelta(hours=timeshift) - timedelta(minutes=3.5)
 
     #else:
-        
+
     #     #check the time it broke and separate the shift in radar table according to shfit
     #     mask = radar.index[selection] > pd.to_datetime(table.broken_time[i])
     #     radar['shift'][selection][mask] = radar.index[selection][mask] + timedelta(hours=table.time_shift_2[i]) - timedelta(minutes=3.5)
     #     radar['shift'][selection][~mask] = radar.index[selection][~mask] + timedelta(hours=timeshift) - timedelta(minutes=3.5)
     # else:
     #     radar['shift'][selection] = radar.index[selection] + timedelta(hours=timeshift) - timedelta(minutes=3.5)
-        
+
 #radar = radar.set_index('shift')
 
 
@@ -146,9 +146,6 @@ ax[2].set_ylabel('(m.a.s.l.)')
 
 #%% KI --- plot by file and color by lines
 file_names = radar['File Name'].unique()
-colors = ['aqua', 'beige', 'blue', 'darkgreen', 'ivory',
-          'lime', 'magenta', 'orange', 'plum', 'red',
-          'purple','silver','yellow','yellowgreen']
 
 for file_name in file_names:
     index = radar['File Name'] == file_name
@@ -162,17 +159,18 @@ for file_name in file_names:
     ax.plot(gps_hh.index, gps_hh['easting'], marker='.',linestyle='',label='gps - hand held', color='black')
     ax.plot(radar.index, radar['easting'], marker='o', linestyle='',label='radar-orginal', color='grey')
 
-    line_name = radar[index]['Line Name'].unique()
-    for line_name in line_name:
+    line_names = radar[index]['Line Name'].unique()
+    color = sns.color_palette("flare",n_colors = len(line_names))
+    for j,line_name in enumerate(line_names):
         sub_index = (radar['File Name'] == file_name)&(radar['Line Name'] == line_name)
-        ax.plot(radar.index_shift[sub_index] ,radar['easting'][sub_index],marker='.',linestyle='',label=line_name, color=np.random.choice(colors))
+        ax.plot(radar.index_shift[sub_index] ,radar['easting'][sub_index],marker='.',linestyle='',label=line_name, color=color[j])
 
     ax.set_xlim(date_range)
     ax.set_ylim(-525000,-510000)
     ax.set_ylabel('(m)')
     ax.legend(fontsize=15)
-    
-    
+
+
 #%% KI --- get file names and line names to csv file
 # File Name | Line Name | time shift (filled manually)
 f_name =  []
@@ -212,7 +210,39 @@ ax.set_xlim(date_range)
 ax.set_ylabel('m.a.s.l')
 ax.legend()
 
-
+#%% interpolate z radar from GPS data
+##############################################################################
+#create empty column to insert the provenance of the z data
+radar['z_interp_origin'] = ''
+# bolean index for the 1d interpolation where no rover data is available
+selection_1 = radar.index > pd.to_datetime('2022-08-21 20:00')
+#interpolate data along the radar timeserie using the rover gps z data
+radar['z_interp'] = np.interp(radar.index,
+                        gps_rover.index,
+                        gps_rover.ellipsoida)
+#remove interpolation values where not rover data is available
+radar.z_interp[selection_1] = radar.z_arcticdem[selection_1].values
+radar['z_interp_origin'][selection_1]='Arctic DEM 10m'
+radar['z_interp_origin'][~selection_1]='GPS rover'
+#add arctic dem interpolation values for missing radar positions
+#radar.z_interp[selection_1] =
+fig,ax = plt.subplots(figsize=(40,15))
+#
+date_range = [pd.to_datetime('2022-08-13 00:00'),pd.to_datetime('2022-08-25 00:00')]
+#ax.axvline(radar.index[radar.z_arcticdem.notna()].values, color = 'black')
+ax.plot(radar.index, radar.z_arcticdem, marker='o',linestyle='',label='Arctic DEM',color='black')
+ax.plot(gps_rover.index, gps_rover['ellipsoida'], marker='o',linestyle='',label='GPS rover',color='grey')
+ax.plot(radar.index ,radar['Z - Elevat'], marker='.',linestyle='',label='radar', color='cyan')
+ax.plot(radar.index[radar['z_interp_origin']=='GPS rover'] ,
+        radar.z_interp[radar['z_interp_origin']=='GPS rover'],
+        marker='.',linestyle='',label='interpolated with rover', color='orangered')
+ax.plot(radar.index[radar['z_interp_origin']=='Arctic DEM 10m'] ,
+        radar.z_interp[radar['z_interp_origin']=='Arctic DEM 10m'],
+        marker='.',linestyle='',label='interpolated with Arctic DEM', color='orange')
+ax.set_xlim(date_range)
+#ax[0].set_ylim(-525000,-510000)
+ax.set_ylabel('m.a.s.l')
+ax.legend()
 
 
 
